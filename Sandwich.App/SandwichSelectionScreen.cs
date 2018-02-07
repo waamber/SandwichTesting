b82e8c0932e101040c639ef4a77d5b3aaae62b4a
waamber <amberastuart@aol.com>
2018-02-06 21:25:23 -0600

    some tests

---
+++ Sandwich.App/SandwichSelectionScreen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandwich.App
+{
+    public class SandwichSelectionScreen
+    {
+        Sandwich _sandwich = new Sandwich();
+        
+        public void AddTomatoes(int numberOfTomatoes)
+        {
+            _sandwich.Tomatoes += numberOfTomatoes;
+        }
+
+        public Sandwich BuildSandwich()
+        {
+            return _sandwich;
+        }
+
+        public void AddOnions()
+        {
+            _sandwich.Onions = true;
+        }
+    }
+
+    public class Sandwich
+    {
+        public int Tomatoes { get; set; }
+        public bool Onions { get; set; }
+    }
+}
